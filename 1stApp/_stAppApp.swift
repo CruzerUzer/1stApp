7d02616028ce5e6b2cc201495545cb7c7030d7f2
Adam Faris <medlem@faris.se>
2026-01-10 15:29:36 +0100

    Initial Commit

---
+++ 1stApp/_stAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  _stAppApp.swift
+//  1stApp
+//
+//  Created by Adam Faris on 2026-01-10.
+//
+
+import SwiftUI
+
+@main
+struct _stAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
